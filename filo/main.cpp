193be9017b44756a41ade16d1148a660eb60ea0b
Test1Denis <ddd183a_1@mail.ru>
2023-01-31 22:04:27 +0300

    частично посмотрел

--- filo/main.cpp
+++ filo/main.cpp
@@ -1,14 +1,15 @@
-#include "fifo.h"
 #include <iostream>
+
+#include "fifo.h"
 
 int main(){
     struct fifo test;
     initBuffer(&test, 3);
-    fillRandom(&test);
-    showMass(&test);
-//    getFromEnd(&test, );
+    fillRandom(&test);	//это что-то новое....
+    showMass(&test);	//эта функция не применима для демонстрации работы fifo
+//    getFromEnd(&test, );	//комментировать код и его выкладывать на в репозиторий вообще строго запрещено... это на будущее....
 //    putFromEnd();
     showMass(&test);
-    flush(&test);
+    flush(&test);	
     return 0;
 }
